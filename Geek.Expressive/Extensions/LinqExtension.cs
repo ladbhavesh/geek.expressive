52885730e1941d8466f987a987bc0280c8089e1d
ladbhavesh <lad.bhavesh@gmail.com>
2019-11-16 16:45:16 +0530

    LINQ extestions

---
+++ Geek.Expressive/Extensions/LinqExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geek.Expressive.Extensions
+{
+    public static class LinqExtension
+    {
+
+        public static IEnumerable<T> SafeWhere<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            if (collection == null)
+                return Enumerable.Empty<T>();
+
+            return collection.Where(predicate);
+        }
+        public static bool SafeAny<T>(this IEnumerable<T> collection)
+        {
+            if (collection == null)
+                return false;
+
+            return collection.Any();
+        }
+
+        public static bool SafeAny<T>(this IEnumerable<T> collection, Func<T,bool> predicate)
+        {
+            if (collection == null)
+                return false;
+
+            return collection.Any(predicate);
+        }
+
+        public static List<T> ToSafeList<T>(this IEnumerable<T> collection)
+        {
+            if (collection == null)
+                return new List<T>();
+
+            return collection.ToList();
+        }
+    }
+}
---
+++ Geek.Expressive/Extensions/ObjectExtesion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Geek.Expressive.Extensions
+{
+    public static class ObjectExtesion
+    {
+        public static void MustNotBeNull(this object value, string parameterName)
+        {
+            if (value == null)
+                throw new ArgumentNullException($"{parameterName} mustnot be null.");
+        }
+    }
+}
